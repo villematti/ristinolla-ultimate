6bc7e3d98bc8ee2eec58451402bc4eed87898c31
Ville-Matti Hakanpää <villematti@villematti.info>
2015-12-20 20:41:37 +0200

    Initial Commit

---
+++ RistinollaUltimate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RistinollaUltimate
+//
+//  Created by Ville-Matti Hakanpää on 20/12/15.
+//  Copyright © 2015 Ville-Matti Hakanpää. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
